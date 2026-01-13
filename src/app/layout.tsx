74d38b6c5f128ce454984c38115879eee5a38a17
NestcodeShield <chaoshorizon2000@gmail.com>
2026-01-13 18:26:33 -0400

    favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,28 @@ export const metadata: Metadata = {
     locale: 'ru_RU',
     type: 'website',
   },
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    // Это для Android (из manifest)
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest', // Путь к вашему манифесту
   verification: {
     yandex: "2c25793a465d554d",
     // Если понадобится Google в будущем, добавьте здесь: google: "код"
